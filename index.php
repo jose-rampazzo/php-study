ac67bc77a5161d2aeade37bd484cf1b0322452dd
jose-rampazzo <jh1997sl@gmail.com>
2024-05-07 14:35:47 -0300

    Novas formas de consultar usuarios no banco

--- index.php
+++ index.php
@@ -6,10 +6,24 @@ require_once("config.php");
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 //echo json_encode($usuarios);
 
+//carrega um usuário]
+/*
 $root = new Usuarios();
-
 $root->loadById(3);
-
 echo $root;
+*/
 
+//carrga uma lista de usuários
+//$lista = Usuarios::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuarios::search("jo");
+//echo json_encode($search);
+
+//carrega um usuário usando o login e a senha
+$usuario = new Usuarios();
+$usuario->login("ichikawa", "kaiju*8");
+
+echo $usuario;
 ?>
